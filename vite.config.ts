12e1fae2347ed2d2f76a41b8014361f497455535
Alvin Liu <liukui@tomo.inc>
2025-07-26 00:01:13 +0800

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  },
   build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          charts: ['lightweight-charts'],
-          network: ['axios']
-        }
-      }
-    }
+    outDir: 'dist'
   }
 }) 
